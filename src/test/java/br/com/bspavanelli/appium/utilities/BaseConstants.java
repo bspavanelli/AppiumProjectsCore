8242fe7ac0019c421da85b1a33bef6750dd3f127
bruno.pavanelli <bruno.pavanelli@000047-wl.lab.dextra.com.br>
2019-10-30 09:03:37 -0300

    Movido alguns parametros definidos no DriverFactory para o BaseConstants

--- src/test/java/br/com/bspavanelli/appium/utilities/BaseConstants.java
+++ src/test/java/br/com/bspavanelli/appium/utilities/BaseConstants.java
@@ -6,7 +6,6 @@ import br.com.bspavanelli.appium.utilities.enums.OS;
 
 public class BaseConstants {
 
-	//	public static OS executionOS = OS.ANDROID;
 	public static OS executionOS = OS.getOS(System.getProperty("executionOS"));
 
 	public static final String BASE_PACKAGE = "br.com.app";
@@ -17,5 +16,13 @@ public class BaseConstants {
 	public static final int DEFAULT_MAX_SCROLLS_TO_FIND_ELEMENT = 5;
 
 	public static final Duration DEFAULT_FACTORY_DURATION = Duration.ofSeconds(DEFAULT_IMPLICITLY_WAIT);
+
+	public static final String APP_DIR = System.getProperty("user.dir") + "/src/test/resources/";
+
+	public static final String ANDROID_APP = APP_DIR + "livelo-remoteUatDebug.apk";
+	public static final String IOS_APP = APP_DIR + "Livelo.app";
+
+	public static final String UDID = "BE50D29B-96AB-458A-9E45-BB27F3270CD0";
+//	public static final String UDID = "5E324623-793C-44E3-9258-56A742135748"; // Jenkins
 
 }
--- src/test/java/br/com/bspavanelli/appium/utilities/DriverFactory.java
+++ src/test/java/br/com/bspavanelli/appium/utilities/DriverFactory.java
@@ -1,6 +1,5 @@
 package br.com.bspavanelli.appium.utilities;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -30,15 +29,11 @@ public class DriverFactory {
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 		switch (BaseConstants.executionOS) {
 			case ANDROID:
-				File classpathRoot = new File(System.getProperty("user.dir"));
-				File appDir = new File(classpathRoot, "/src/test/resources");
-				File app = new File(appDir, "nome_do_app.apk");
-
 				desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
 				desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "emulator-5554");
 				desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 				desiredCapabilities.setCapability(AndroidMobileCapabilityType.AUTO_GRANT_PERMISSIONS, true);
-				desiredCapabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+				desiredCapabilities.setCapability(MobileCapabilityType.APP, BaseConstants.ANDROID_APP);
 				// desiredCapabilities.setCapability("appPackage", BaseConstants.BASE_PACKAGE);
 				desiredCapabilities.setCapability("appActivity", BaseConstants.BASE_ACTIVITY);
 
@@ -55,9 +50,6 @@ public class DriverFactory {
 
 				break;
 			case IOS:
-				classpathRoot = new File(System.getProperty("user.dir"));
-				appDir = new File(classpathRoot, "/src/test/resources/");
-				app = new File(appDir, "nome_do_app.app");
 				desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "IphoneXUiTest");
 				desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "XCUITest");
 				desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "IOS");
@@ -67,11 +59,11 @@ public class DriverFactory {
 				desiredCapabilities.setCapability(MobileCapabilityType.CLEAR_SYSTEM_FILES, true);
 				desiredCapabilities.setCapability(IOSMobileCapabilityType.AUTO_ACCEPT_ALERTS, true);
 				desiredCapabilities.setCapability(IOSMobileCapabilityType.USE_NEW_WDA, true);
-				desiredCapabilities.setCapability(MobileCapabilityType.APP, app.getAbsolutePath());
+				desiredCapabilities.setCapability(MobileCapabilityType.APP, BaseConstants.IOS_APP);
 				desiredCapabilities.setCapability("unicodeKeyboard", true);
 				desiredCapabilities.setCapability("resetKeyboard", true);
 				desiredCapabilities.setCapability("connectHardwareKeyboard", true);
-				desiredCapabilities.setCapability("udid", "BE50D29B-96AB-458A-9E45-BB27F3270CD0");
+				desiredCapabilities.setCapability("udid", BaseConstants.UDID);
 				try {
 					driver = new IOSDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
 				} catch (MalformedURLException e) {
